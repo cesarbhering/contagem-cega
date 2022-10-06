9f20bcfc080dfa16e676ffa137ef0c4a0c188396
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2022-10-05 23:37:30 -0300

    adiciona el-input-number para a contagem do funcionario

--- components/ProductsTable.vue
+++ components/ProductsTable.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <el-table
-      height="750"
-      :data="actualStockValues"
-      border
-      style="width: 100%"
-    >
+    <el-table height="750" :data="actualStockValues" border style="width: 100%">
       <el-table-column
         v-for="column in displayColunmns"
         :key="column"
@@ -13,6 +8,17 @@
         :label="column"
         :width="260"
       />
+      <el-table-column label="SALDO ATUAL INFORMADO" width="180">
+        <template slot-scope="scope">
+          <el-input-number
+            v-model="traineeInsertedValues[scope.$index]"
+            :min="0"
+            :max="999999"
+            :step="1"
+            :precision="0"
+          />
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -22,17 +28,27 @@ import { mapFields } from 'vuex-map-fields'
 export default {
   name: 'ProductsTable',
 
+  data () {
+    return {
+      traineeInsertedValues: []
+    }
+  },
+
   computed: {
     ...mapFields('products', ['displayColunmns', 'actualStockValues'])
+  },
+
+  beforeUpdate () {
+    this.traineeInsertedValues = this.actualStockValues.map((item) => {
+      return 0
+    })
   }
 }
 </script>
 
 <style scoped>
-
 .table-column {
   text-align: center;
   background-color: red;
 }
-
 </style>
--- store/products.js
+++ store/products.js
@@ -3,7 +3,9 @@ import { getField, updateField } from 'vuex-map-fields'
 export const state = () => ({
   displayColunmns: ['COD', 'DENOMINACAO', 'COMPLEM', 'UN', 'SALDO ATUAL'],
 
-  actualStockValues: [{}]
+  actualStockValues: [],
+
+  correctStockValues: [{}]
 })
 
 export const getters = {
@@ -14,11 +16,18 @@ export const mutations = {
   updateField,
   SET_ACTUAL_STOCK_VALUES (state, payload) {
     state.actualStockValues = payload
+  },
+  SET_CORRECT_STOCK_VALUES (state, payload) {
+    state.correctStockValues = payload
   }
 }
 
 export const actions = {
   setActualStockValues ({ commit }, payload) {
     commit('SET_ACTUAL_STOCK_VALUES', payload)
+  },
+
+  setCorrectStockValues ({ commit }, payload) {
+    commit('SET_CORRECT_STOCK_VALUES', payload)
   }
 }
